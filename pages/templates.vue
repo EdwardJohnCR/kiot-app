0d4a12ec9769ec89e22e4cd573d5192fe22eaef1
EdwardJohnCR <edwardjohncr@gmail.com>
2023-05-28 13:11:30 -0600

    inicio de creacion de widget

--- pages/templates.vue
+++ pages/templates.vue
@@ -1,25 +1,62 @@
 <template>
-    <div>
-      <Iotindicator></Iotindicator>
-      <button @click="sendData()">SEND</button>
-    </div>
-</template>
+  <div class="row">
+    <Iotbutton :config="configButton"></Iotbutton>
+    <Iotindicator :config="configIndicator"></Iotindicator>
 
+    <button @click="sendData1()">SEND</button>
+  </div>
+
+
+</template>
 <script>
-    export default {
-        data(){
-            return {
-                value:false
-            }
+export default {
+  data() {
+    return {
+      value: false,
+      configButton: {
+        userId: "userid",
+        selectedDevice: {
+          name: "Home",
+          dId: "8888",
+          templateName: "Power Sensor",
+          templateId: "984237562348756ldksjfh",
+          saverRule: false
         },
-        methods: {
-            sendData(){
-                this.value = !this.value;
-                const toSend = {
-                    value: this.value
-                }
-                this.$nuxt.$emit('widget-topic', toSend );
-            }
-        }
+        variableFullName: "Pump",
+        variable: "var1",
+        icon: "fa-sun",
+        column: "col-6",
+        widget: "indicator",
+        class: "danger",
+        message: "{'fanstatus': 'stop'}"
+      },
+      configIndicator: {
+        userId: "userid",
+        selectedDevice: {
+          name: "Home",
+          dId: "8888",
+          templateName: "Power Sensor",
+          templateId: "984237562348756ldksjfh",
+          saverRule: false
+        },
+        variableFullName: "Pump",
+        variable: "var1",
+        icon: "fa-sun",
+        column: "col-6",
+        widget: "indicator",
+        class: "success"
+      }
+    };
+  },
+  
+  methods: {
+    sendData1() {
+      this.value = !this.value;
+      const toSend = {
+        value: this.value
+      };
+      this.$nuxt.$emit("userid/8888/var1/sdata", toSend);
     }
+  }
+};
 </script>
