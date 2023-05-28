571bbaa99d442339c61cc9dbd0237fe4a26c9358
EdwardJohnCR <edwardjohncr@gmail.com>
2023-05-27 23:09:55 -0600

    envio de bolean .

--- pages/templates.vue
+++ pages/templates.vue
@@ -1,7 +1,25 @@
 <template>
     <div>
-        <h2>
-            Templates
-        </h2>
+      <Iotindicator></Iotindicator>
+      <button @click="sendData()">SEND</button>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return {
+                value:false
+            }
+        },
+        methods: {
+            sendData(){
+                this.value = !this.value;
+                const toSend = {
+                    value: this.value
+                }
+                this.$nuxt.$emit('widget-topic', toSend );
+            }
+        }
+    }
+</script>
